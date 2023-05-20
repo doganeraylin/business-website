9999e955ceb02be9d6cd7029fbe3cd05f85d6b9d
doganeraylin <doganeraylinn@gmail.com>
2023-05-20 10:23:02 +0200

    created product page layout

---
+++ pages/products.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+    <img class="container__img" src="assets/products/banner.png">
+    <h1 class="container__title">all products</h1>
+    <p class="container__subtitle">explore our captivating collection of handcrafted furniture pieces</p>
+    <ProductCard/>
+    <ProductDetails/>
+</div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/variables.scss";
+@import "@/assets/main.scss";
+
+.container {
+    &__img {
+        max-width: 100%;
+    }
+    &__subtitle {
+        text-align: center;
+    }
+
+}
+</style>
