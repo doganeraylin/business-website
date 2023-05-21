63dcbfe07fe585f38c5b6e65bc16c8afca9e0c20
doganeraylin <doganeraylinn@gmail.com>
2023-05-21 09:17:02 +0200

    added button comp

--- components/Newsletter.vue
+++ components/Newsletter.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="container">
         <h1 class="container__title">Unlock a World of Inspiration:</h1>
-        <h2 class="container__subtitle container__text"> Join Our Newsletter for Design Tips, Trends, and Exclusive Deals.</h2>
-        <div class="container__input container__text">
+        <h2 class="container__subtitle"> Join Our Newsletter for Design Tips, Trends, and Exclusive Deals.</h2>
+        <div class="input_container">
             <input type="email" placeholder="example@example.com">
-            <button>subscribe</button>
+            <Button 
+                buttonClass="primary-style"
+                buttonText="subscribe"
+                clickHandler="goToProducts"
+            />
         </div>
     </div>
 </template>
@@ -22,27 +26,27 @@
         font-weight: lighter;
         margin: 0.5rem auto 1rem auto;
     }
-    &__input {
-        input[type="email"] {
-            padding: 0.5rem 1rem;
-        }
+}
+
+.input_container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    input[type="email"] {
+    padding: 0.8rem 1rem;
+    border: none;
     }
 }
 
 @media (min-width: 1080px) {
     .container {
-    &__title {
-        font-size: 3.5rem;
-    }
-    &__subtitle {
-        font-size: 2.5rem;
-    }
-    &__input {
-        input[type="email"] {
-            padding: 1rem 2.5rem;
+        &__title {
+            font-size: 3.5rem;
+        }
+        &__subtitle {
+            font-size: 2.5rem;
         }
     }
-}
 }
 
 </style>
