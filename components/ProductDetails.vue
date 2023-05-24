6aed366d1a7399991fabe8a247390b8b38de0134
doganeraylin <doganeraylinn@gmail.com>
2023-05-24 08:41:43 +0200

    added props, rendered specifications

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -1,29 +1,35 @@
 <template>
+    <Navbar/>
     <div class="product_card_container">
         <div class="product_card">
-            <img class="product_card__img" src="assets/products/chair.png">
+            <img class="product_card__img" :src="product.img">
             <div class="product_details">
-                <p class="product_details__model">Model 2980</p>
-                <p class="product_details__name">Serenity Seat</p>
-                <p class="product_details__description">Indulge in the ultimate relaxation with our Serenity Seat, an exquisite piece of furniture designed to bring serenity and comfort to your living space. With its sleek and modern design, the Serenity Seat seamlessly blends into any interior décor, creating a serene oasis where you can unwind and rejuvenate.</p>
+                <p class="product_details__model">Model {{product.model}}</p>
+                <p class="product_details__name">{{product.name}}</p>
+                <p class="product_details__description">{{product.description}}</p>
                 <p>Materials</p>
+                <p class="product_details__materials">{{product.materials}}</p>
                 <p>Specifications</p>
+                <ul class="specifications">
+                    <li v-for="spec in product.specifications" :key="spec.width">
+                        Width: {{ spec.width }}
+                        Depth: {{ spec.depth }}
+                        Height: {{ spec.height }}
+                    </li>
+                </ul>
             </div>
         </div>
-        <div class="links">
-            <Button 
-                to="/where-to-buy"
-                buttonClass="default-style"
-                buttonText="find in store"
-                clickHandler="goToProducts"
-            />
             <div class="wishlist">
                 <img src="/assets/empty-fav.png" class="wishlist__icon">
                 <p class="wishlist__text">Add to wishlist</p>
             </div>
         </div>
-    </div>
 </template>
+
+
+<script setup>
+  const { product } = defineProps(['product'])
+</script>
 
 
 
@@ -34,14 +40,17 @@
 .product_card_container {
     max-width: 800px;
     margin: 0 auto;
+    border: 1px solid green;
 }
 
 .product_card {
+    border: 1px solid gray;
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 0 1rem;
     max-width: 325px;
+    margin: 0 auto;
     &__img {
         max-width: 325px;
         border: 1px solid $light-brown;
@@ -56,24 +65,25 @@
     &__name {
         font-weight: bold;
     }
-    &__description {
+    &__description,
+    &__materials {
         font-size: 0.8rem;
     }
 }
 
-.links,
+.specifications {
+    max-width: 100px;
+    padding: 0;
+    li {
+        list-style: none;
+        font-size: 0.8rem;
+    }
+}
+
 .wishlist {
     display: flex;
-    justify-content: space-between;
     align-items: center;
-}
-
-.links {
-    max-width: 325px;
-    margin: 0 auto;
-}
-
-.wishlist {
+    justify-content: center;
     color: $light-brown;
     text-decoration: underline;
     &__icon {
@@ -81,7 +91,6 @@
         margin: 0 0.5rem 0 0;
     }
 }
-
 
 @media (min-width: 768px) {
     .product_card_container {
