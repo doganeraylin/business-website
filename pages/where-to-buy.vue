a48be2c54877cfcf2d3cb1c012982705da5ec341
doganeraylin <doganeraylinn@gmail.com>
2023-05-25 10:29:15 +0200

    created filtering

--- pages/where-to-buy.vue
+++ pages/where-to-buy.vue
@@ -1,7 +1,40 @@
 <template>
-<div class="container">
+  <div class="container">
     <Navbar/>
-    <FindDealer/>
+    <img class="container__img" src="/assets/findDealer/banner.png">
+    <h1 class="container__title">find a dealer</h1>
+    <div class="sortby_container">
+        <div v-for="country in dealers" :key="country" class="options_container">
+          <label>
+            <input :value="country.country" v-model="filteredDealers" type="checkbox"/>
+            <span >{{ country.country}}</span>
+          </label>
+        </div>
+    </div>
+    <FindDealer :filteredDealers="filteredDealers" :dealers="dealers"/>
     <Footer/>
-</div>
+  </div>
 </template>
+
+
+<script setup>
+  const { data: dealers } = await useFetch('https://business-website-json.vercel.app/dealersList')
+  const allDealers = ref([dealers])
+  const filteredDealers = ref([])
+</script>
+
+
+<style lang="scss">
+@import "@/assets/variables.scss";
+@import "@/assets/main.scss";
+
+.container {
+    &__img {
+        max-width: 100%;
+    }
+    &__title {
+        margin: 1rem auto;
+    }
+}
+
+</style>
