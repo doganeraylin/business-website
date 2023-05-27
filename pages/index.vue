dbe56ee7f5acd48320fb005b24a6ccdf920c28cb
doganeraylin <doganeraylinn@gmail.com>
2023-05-27 09:58:57 +0200

    rendered home page components

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <Navbar/>
+    <Hero/>
+    <BrandValues/>
+    <CTA/>
+    <Newsletter/>
+    <Footer/>
+    <!-- <Confirmation/> -->  
+</template>
+
+<style>
+@import "@/assets/main.scss";
+@import "@/assets/variables.scss";
+</style>
