6ae49e55bfcb43ca0bbb0414ebb10bb8a36302c0
doganeraylin <doganeraylinn@gmail.com>
2023-06-02 16:48:22 +0200

    added navbar, fixed positioning

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,9 @@
 <template>
+    <Navbar/>
     <div class="container">
         <div class="story_container">
             <h1 class="container__title">our story</h1>
-            <p class="container__story">At [Your Brand], established in [Year] in [Place], we are driven by our passion for creating timeless furniture that blends functionality, aesthetics, and sustainability. From our humble beginnings in a small workshop, we have grown into a recognized name, guided by our unwavering commitment_ to exceptional craftsmanship and innovative design. Each piece in our collection tells a story, crafted with care and attent_ion to detail, bringing beauty and inspiration to homes worldwide</p>
+            <p class="container__story">At Lumière, established in 2008 in Copenhagen, we are driven by our passion for creating timeless furniture that blends functionality, aesthetics, and sustainability. From our humble beginnings in a small workshop, we have grown into a recognized name, guided by our unwavering commitment_ to exceptional craftsmanship and innovative design. Each piece in our collection tells a story, crafted with care and attent_ion to detail, bringing beauty and inspiration to homes worldwide</p>
         </div>
         <AboutImgLayout/>
         <AboutStatement/>
@@ -19,13 +20,12 @@
     &__story_wrapper {
         display: flex;
         flex-direction: column;
-        align-items: cent_er;
+        align-items: center;
     }
     &__story {
         padding: 0 1rem;
         max-width: 750px;
         margin: 1rem auto;
-
     }
 }
 
