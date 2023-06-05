1fd2b3788bab179cc5adec60de172cb2ccba0d93
doganeraylin <doganeraylinn@gmail.com>
2023-06-05 10:17:40 +0200

    moved main stylesheet to here

--- app.vue
+++ app.vue
@@ -5,3 +5,10 @@
     </NuxtLayout>
   </div>
 </template>
+
+<style lang="scss">
+@import "@/assets/main.scss";
+@import "@/assets/variables.scss";
+@import url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+</style>
